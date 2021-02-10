d24408165c4b0bd0ea13841150f007df3802dd0f
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-02-11 00:33:56 +0300

    routerlink added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { SingleProductPageComponent } from './components/single-product-page/single-product-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { NgModule } from '@angular/core';
@@ -5,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { KonnectComponent } from './components/konnect/konnect.component';
+import { SingleShopProductsListComponent } from './components/single-shop-products-list/single-shop-products-list.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
@@ -12,6 +14,8 @@ const routes: Routes = [
   { path: 'konnect', component: KonnectComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'myaccount/sign-in', component: LoginComponent },
+  { path: 'product-details', component: SingleProductPageComponent },
+  { path: 'shop/shops-products', component: SingleShopProductsListComponent }
 ];
 
 @NgModule({
