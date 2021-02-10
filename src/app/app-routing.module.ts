3bbd6129188689a4a171c878f31a634f63ca79ab
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-02-11 00:26:00 +0300

    navigation between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { LoginComponent } from './components/login/login.component';
+import { OrdersComponent } from './components/orders/orders.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { KonnectComponent } from './components/konnect/konnect.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'konnect', component: KonnectComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'myaccount/sign-in', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
