b7cdd2192639c5f8a132fcab87e225d7f57e2d30
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-02-15 19:52:17 +0300

    banners updated and some colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,9 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers: [
+    // {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
